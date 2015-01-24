2b76cdaf7b15f7cdd48f4fc9efbd2c97c68f6d50
John Goodall <jgoodall@gmail.com>
2015-01-24 13:48:12 -0500

    changed file partial for result list from "node" to "result"

--- client/partials/search-result.js
+++ client/partials/search-result.js
@@ -2,7 +2,7 @@ var View = require('ampersand-view');
 var templates = require('../templates');
 
 module.exports = View.extend({
-  template: templates.includes.node,
+  template: templates.includes.result,
   bindings: {
     'model.gid': '[data-hook~=gid]',
     'model.name': '[data-hook~=name]',

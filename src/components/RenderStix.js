66f3778e4c5cefecdbbefa649d6d26e081f90754
Maria Vincent <mvincen2@vols.utk.edu>
2016-08-12 16:32:01 -0400

    rendered stix report

---
+++ src/components/RenderStix.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+// import stix from '../stix-to-html/stix'
+
+class RenderStix extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    )
+  }
+}
+
+RenderStix.propTypes = {
+  report: React.PropTypes.object
+}
+
+const mapStateToProps = function (store) {
+  return {
+    report: store.report
+  }
+}
+
+export default connect(mapStateToProps)(RenderStix)

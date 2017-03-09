91d57c8af0e5409df96a21555b4fb02bfc8eb441
Maria Vincent <mvincen2@vols.utk.edu>
2017-03-08 19:57:32 -0500

    cytoscape stix ontology representation component

---
+++ components/Graph.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import CytoscapeGraph from './vis/src/CytoscapeGraph'
+
+let stuccoOntology = require('./vis/data/cytoscape-graph/data.json');
+
+class Graph extends React.Component {
+  constructor () {
+  	super()
+  }
+  render () {
+    return ( 
+      <div > 
+        <CytoscapeGraph graph={stuccoOntology} />
+      </div>
+    ) 
+  }
+}
+
+export default Graph
+                          

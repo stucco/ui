2f9e4e7a771111a5ae4e498bd2e9b4931b70d97e
Maria Vincent <mvincen2@vols.utk.edu>
2017-03-08 20:03:58 -0500

    added filtering options: source and search fields in drop list

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -7,18 +7,34 @@ import Link from './Link'
 
 import { SET_VERTEX } from '../redux/actions'
 
+  const properties = {
+      name: 'Name',
+      vertexType: 'Vertex Type',
+      observableType: 'Observable Type',
+      description: 'Description',
+      source: 'Source',
+      ipInt: 'IP Integer',
+      startIP: 'Start IP',
+      endIP: 'End IP',
+      startIPInt: 'Start IP Integer',
+      endIPInt: 'End IP Integer'
+    }
+
 class SearchBox extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       renderResult: false,
-      key: '',
+      key: 'name',
+      title: 'Name',
       value: '',
-      page: 0
+      page: 0,
+      source: 'stucco'
     }
     this.transition = this.transition.bind(this)
     this.handKeyPress = this.handKeyPress.bind(this)
     this.focus = this.focus.bind(this)
+    this.radioCheck = this.radioCheck.bind(this)
   }
 
   focus () {
@@ -32,31 +48,56 @@ class SearchBox extends React.Component {
   }
 
   transition (event) {
-    let inputText = ReactDOM.findDOMNode(this.textInput).value
-    let input = inputText.split(/\s*=\s*/)
-    let key = input[0]
-    let value = input[1]
-    if (value.includes(":")) {
-      value = '\"' + value + '\"'
-    }
-    let url = "/resultslist/search/" + key + "=" + value + "&page=0&pageSize=20"
+    let value = ReactDOM.findDOMNode(this.textInput).value
+    let url = "/resultslist/search/" + this.state.source + "/" + this.state.key + "=" + value + "&page=0&pageSize=20"
     event.preventDefault();
     history.push({ pathname: url });
+  }
+
+  radioCheck (event) {
+    console.log("in console ... " , event)
+    console.log(event.currentTarget.value)
+    this.setState({source: event.currentTarget.value})
+  }
+
+  handleClick (event) {
+    console.log(event)
+    this.setState({title: properties[event]})
+    this.setState({key: event})
   }
 
   render () {
     return (
       <div className='container-fluid'>
         <div className='row'>
-          <form className='form-horizontal' autoComplete='on' >
+          <form className='form-horizontal' autoComplete='on' role="form">
             <div className='search-query-form'>
               <div className={cx('col-xs-6', 'col-xs-offset-2')}>
                 <div className='form-group'>
-                  <div style={{dataAnddomDisplay: '', dataAnddomHidden: 'true', display: 'none'}}>
-                    <div className={cx('alert', 'alert-danger')}>This field is required.</div>
+                  <div className='input-group'>
+                    <div className={cx("radio", "input-group-addon")} style={{borderColor: '#2e6da4', backgroundColor: '#337ab7', color: '#fff'}}>
+                      <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="stucco" defaultChecked  onChange={this.radioCheck}/>
+                        Stucco
+                      </label>
+                    </div>
+                    <div className={cx("radio", "input-group-addon")} style={{borderColor: '#2e6da4', backgroundColor: '#337ab7', color: '#fff'}} >
+                      <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios2" value="situ" onChange={this.radioCheck} />
+                        Situ
+                      </label>
+                    </div>
+                    <div className={cx("btn", "btn-default", "dropdown-toggle", "dropdown", 'input-group-addon')} style={{borderColor: '#2e6da4'}}>
+                      <div type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" >
+                        &nbsp;{ this.state.title }&nbsp;
+                        <span className="caret"></span>
+                      </div>
+                      <ul className="dropdown-menu" aria-labelledby="dropdownMenu1" >
+                        {Object.keys(properties).map((property, i) => <li key={i} onClick={this.handleClick.bind(this, property)}> &nbsp;&nbsp;{properties[property]}</li>)}
+                      </ul>
+                    </div>
+                    <input autoFocus='autoFocus' className={cx('input-lg', 'form-control')} name='search' placeholder='Search...' type='text' ref={(input) => this.textInput = input} onKeyPress={this.handKeyPress} />
                   </div>
-                  <label className='sr-only'>Search query</label>
-                  <input autoFocus='autoFocus' className={cx('input-lg', 'form-control')} name='search' placeholder='Search...' type='text' ref={(input) => this.textInput = input} onKeyPress={this.handKeyPress} />
                 </div>
               </div>
               <div className='col-xs-2' >

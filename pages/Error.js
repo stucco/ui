a7ae0a37b291673034abbeee66dcdf74e54faedf
Maria Vincent <mvincen2@vols.utk.edu>
2017-03-21 11:37:59 -0400

    added error page

--- pages/Error.js
+++ pages/Error.js
@@ -25,19 +25,20 @@ class ErrorPage extends React.Component {
 
     const [code, title] = this.props.error && this.props.error.status === 404 ?
       ['404', 'Page not found'] :
-      ['Error', 'Oups, something went wrong']
+      ['Error', 'Oups, something went wrong ... check your search criterias.']
 
     return (
-      <div className={s.container}>
-        <main className={content}>
-          <h1 className={code}>{code}</h1>
-          <p className={title}>{title}</p>
+      <div className='container'>
+        <main className='content'>
+          <h1 className='code'>{code}</h1>
+          <p className='title'>{title}</p>
           {code === '404' &&
-            <p className={text}>
-              The page you're looking for does not exist or an another error occurred.
+            <p className='text'>
+              The page you are looking for does not exist or an another error occurred.
+              Check your search criterias.
             </p>
           }
-          <p className={text}>
+          <p className='text'>
             <a href="/" onClick={this.nodegoBack}>Go back</a>, or head over to the&nbsp;
             <Link to="/">home page</Link> to choose a new direction.
           </p>
